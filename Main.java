e2b16360612400eb9d1b60a63b9b9fd47a14302c
monisha <djmonisha.2022@gmail.com>
2023-12-06 19:41:21 +0530

    optimal approach using recursion

--- Main.java
+++ Main.java
@@ -1,20 +1,24 @@
 
 import java.util.*;
 public class Main {
-
-    
-    static int sort(int arr[]) {
-    Arrays.sort(arr);
-    return arr[arr.length - 1];
-  }
  
   public static void main(String args[]) {
  
     int arr1[] =  {2,5,1,3,0};
-    System.out.println("The Largest element in the array is: " + sort(arr1));
-   
-   // int arr2[] =  {8,10,5,7,9};
-   // System.out.println("The Largest element in the array is: " + sort(arr2));
+    System.out.println("The Largest element in the array is: "+findLargestElement(arr1));
+    
+ 
+    int arr2[] =  {8,10,5,7,9};
+    System.out.println("The Largest element in the array is: "+findLargestElement(arr2));
+    
   }
-  
+  static int findLargestElement(int arr[]) {
+    int max= arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max= arr[i];
+      }
+    }
+    return max;
+  }
 }
