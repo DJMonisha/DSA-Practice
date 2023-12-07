cc928cd8dcbb3d54d2433ecf455d04b77faaf3a3
monisha <djmonisha.2022@gmail.com>
2023-12-07 13:42:03 +0530

    move zeroes to the end

---
+++ move zeroes/Main.java
@@ -0,0 +1,44 @@
+//Optimal approach
+
+import java.util.*;
+
+public class Main {
+    public static int[] moveZeros(int n, int []a) {
+        int j = -1;
+        //to place the pointer j:
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        //for no non-zero elements:
+        if (j == -1) return a;
+
+        //to Move the pointers i and j
+        //and to swap accordingly:
+        for (int i = j + 1; i < n; i++) {
+            if (a[i] != 0) {
+                //swap a[i] & a[j]:
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                j++;
+            }
+        }
+        return a;
+    }
+    //static main function
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+        int n = 10;
+        int[] ans = moveZeros(n, arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println("");
+    }
+}
+
