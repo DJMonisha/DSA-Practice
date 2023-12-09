dadec71ecbf532d784780c2ec9b9854a9dfe407c
monisha <djmonisha.2022@gmail.com>
2023-12-09 23:28:06 +0530

    TwoSum: Check if pair of sum exist

---
+++ Two Sum/twoSum.java
@@ -0,0 +1,58 @@
+//Naive or Brute-Force Approach
+
+//Code Variant 1:
+
+import java.util.*;
+
+public class twoSum {
+    public static String twoSum(int n, int []arr, int target) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer" + ans);
+    }
+
+}
+/* 
+
+Code Variant 2:
+
+ *import java.util.*;
+
+public class Main {
+    public static int[] twoSum(int n, int []arr, int target) {
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        int[] ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 2: [" + ans[0] + ", "
+                           + ans[1] + "]");
+    }
+
+} 
+ */
