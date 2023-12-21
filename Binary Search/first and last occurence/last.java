a9df1522620f8e80eff01616159eef411cc6a689
monisha <djmonisha.2022@gmail.com>
2023-12-21 23:37:31 +0530

    Count Occurences using Binary Search

--- Binary Search/first and last occurence/last.java
+++ Binary Search/first and last occurence/last.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 //Using Binary Search
 
-public class tUf {
+public class last {
 
     public static int firstOccurrence(ArrayList<Integer> arr, int n, int k) {
         int low = 0, high = n - 1;
