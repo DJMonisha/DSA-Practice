9c82c2ddb1470a3816da893434dfca31b1f899f8
monisha <djmonisha.2022@gmail.com>
2023-12-29 20:23:34 +0530

    1614. Maximum Nesting Depth of the Parentheses

---
+++ strings/leetcode/Maximum Nesting Depth of the Parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+        int ans=0;
+        int temp=0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                temp++;
+            }
+            if(s.charAt(i)==')'){
+                temp--;
+            }
+            ans=Math.max(ans,temp);
+        }
+        return ans;
+    }
+}
